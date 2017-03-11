da1a633bfd76d527759d93670cf8f8f0cbb169b8
Simon Kågedal Reimer <simon@simons-mbp-4>
2017-03-11 10:40:58 +0100

    Initial Commit

---
+++ SKRBatchUpdates/SKRBatchUpdates.h
@@ -0,0 +1,19 @@
+//
+//  SKRBatchUpdates.h
+//  SKRBatchUpdates
+//
+//  Created by Simon Kågedal Reimer on 2017-03-11.
+//  Copyright © 2017 Simon Kågedal Reimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKRBatchUpdates.
+FOUNDATION_EXPORT double SKRBatchUpdatesVersionNumber;
+
+//! Project version string for SKRBatchUpdates.
+FOUNDATION_EXPORT const unsigned char SKRBatchUpdatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKRBatchUpdates/PublicHeader.h>
+
+
