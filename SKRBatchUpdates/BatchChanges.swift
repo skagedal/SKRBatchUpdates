f32f28cf9994e3925a1a2d6c2fb5c558b9287d96
Simon Kågedal Reimer <simon.reimer@bontouch.com>
2017-03-12 08:54:52 +0100

    Move DataSource to its own file

--- SKRBatchUpdates/BatchChanges.swift
+++ SKRBatchUpdates/BatchChanges.swift
@@ -26,63 +26,6 @@ struct BatchChanges {
     let numberOfItemsInSectionsAfterSectionChanges: [Int]
 }
 
-// MARK: - Data source
-
-public class DataSource<SectionType: Hashable, ItemType: Hashable> {
-
-    public init() {
-    }
-    
-    var sections: [(SectionType, [ItemType])] = []
-
-    var halfTimeItemCounts: [Int]? = nil
-    
-    public func numberOfSections() -> Int {
-        if let rowCounts = halfTimeItemCounts {
-            return rowCounts.count
-        } else {
-            return sections.count
-        }
-    }
-    
-    public func numberOfRows(in section: Int) -> Int {
-        if let rowCounts = halfTimeItemCounts {
-            return rowCounts[section]
-        } else {
-            let (_, items) = sections[section]
-            return items.count
-        }
-    }
-    
-    public func section(at sectionIndex: Int) -> SectionType {
-        let (section, _) = sections[sectionIndex]
-        return section
-    }
-    
-    public func itemForRow(at indexPath: IndexPath) -> ItemType {
-        guard halfTimeItemCounts == nil else {
-            fatalError("Cells are expected to not be accessed in this state")
-        }
-        let (_, items) = sections[indexPath.section]
-        return items[indexPath.item]
-    }
-    
-    public func animate(to sections: [(SectionType, [ItemType])], in tableView: UITableView, with animation: UITableViewRowAnimation) {
-        let changes = BatchChanges(from: self.sections, to: sections)
-        self.sections = sections
-        
-        halfTimeItemCounts = changes.numberOfItemsInSectionsAfterSectionChanges
-        tableView.beginUpdates()
-        tableView.updateSections(for: changes, with: animation)
-        tableView.endUpdates()
-        
-        halfTimeItemCounts = nil
-        tableView.beginUpdates()
-        tableView.updateRows(for: changes, with: animation)
-        tableView.endUpdates()
-    }
-}
-
 // MARK: - Version with not hashables
 
 extension BatchChanges {
